c28d136a69829ee98dd338b921ee7480d7c04bee
Arthur Santana <43881656+arthurk88@users.noreply.github.com>
2022-08-11 17:19:07 -0300

    Update TRANCA_DIGITAL.ino

--- TRANCA_DIGITAL/TRANCA_DIGITAL.ino
+++ TRANCA_DIGITAL/TRANCA_DIGITAL.ino
@@ -9,6 +9,11 @@
 #define colunas   16
 #define linhas    2
 
+#define BUZ 8 // SOM
+#define LEDRED 7 // Define o pino 7 com PWM como RED
+#define LEDGREEN 6 // Define o pino 6 com PWM como GREEN
+#define LEDBLUE 5 // Define o pino 5 com PWM como BLUE
+#define TRANCA 10 // Trava
 // INSTANCIANDO OBJETOS
 LiquidCrystal_I2C lcd(endereco, colunas, linhas);
 
@@ -30,10 +35,12 @@ byte PINOS_COLUNA[COLUNAS] = {30,32,34,36}; //pinos que indicam as colunas do te
 //instancia de Keypad, responsável por capturar a tecla pressionada
 Keypad customKeypad = Keypad( makeKeymap(SIMBOLOS), PINOS_LINHA, PINOS_COLUNA, LINHAS, COLUNAS);
 
-const int ledPin = 10;
+
 const String SENHA = "123456";
 String digitado = "";
 int i = 0;
+
+int red = 255, green = 255, blue = 255;
 
 /*==============================================================================================================*/
 /*==============================================================================================================*/
@@ -43,6 +50,13 @@ void setup() {
   lcd.init(); // INICIA A COMUNICAÇÃO COM O DISPLAY
   lcd.backlight(); // LIGA A ILUMINAÇÃO DO DISPLAY
   lcd.clear(); // LIMPA O DISPLAY
+
+  pinMode(BUZ, OUTPUT); // SOM SAIDA
+  pinMode (LEDRED, OUTPUT); // Pino 7 declarado como saída
+  pinMode (LEDGREEN, OUTPUT); // Pino 6 declarado como saída
+  pinMode (LEDBLUE, OUTPUT); // Pino 5 declarado como saída 
+  pinMode(TRANCA, INPUT);
+
 
   lcd.print("Aguarde.");
   delay(500); // DELAY DE 5 SEGUNDOS
@@ -54,15 +68,13 @@ void setup() {
   delay(500); // DELAY DE 5 SEGUNDOS
   lcd.clear(); // LIMPA O DISPLAY
   
-  pinMode(ledPin, OUTPUT);    // sets the digital pin 13 as output
   Serial.begin(9600);
   Serial.println("teclado 4x4");
   Serial.println("Aguardando...");
   
   lcd.print("Senha :");
   lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
-  
-  digitalWrite(ledPin, HIGH);
+ 
 
  
 }
@@ -78,21 +90,42 @@ void loop() {
     {
       //caso alguma das teclas imprimíveis foi pressionada
       case 'A':
+              digitalWrite(BUZ, HIGH); // ativa o SOM
+              analogWrite (LEDBLUE, 255);
+              delay(500);            // espera por um segundo
+              digitalWrite(BUZ, LOW); // Desativa o SOM
+              analogWrite (LEDBLUE, 0);
+            
               if(digitado == SENHA){
                 digitado = "";
                 i = 0;
-                digitalWrite(ledPin, LOW); // sets the digital pin 13 on
+
+                  
+                pinMode(TRANCA, OUTPUT);
+                
+                digitalWrite(BUZ, HIGH); // ativa o SOM
+                analogWrite (LEDGREEN, 255);
+                delay(500);            // espera por um segundo
+                digitalWrite(BUZ, LOW); // Desativa o SOM
+                analogWrite (LEDGREEN, 0);
+               
                 lcd.clear(); // LIMPA O DISPLAY
                 lcd.print("Liberado!");
                 delay(5000); // DELAY DE 5 SEGUNDOS
+                pinMode(TRANCA, INPUT);
                 //chama o comando para limpar a tela
                 lcd.clear(); // LIMPA O DISPLAY
                 lcd.print("Senha :");
                 lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
-                digitalWrite(ledPin, HIGH);  // sets the digital pin 13 off
+              
               }else{
                 digitado = "";
                 i = 0;
+                digitalWrite(BUZ, HIGH); // ativa o SOM
+                analogWrite (LEDRED, 255);
+                delay(500);            // espera por um segundo
+                digitalWrite(BUZ, LOW); // Desativa o SOM
+                analogWrite (LEDRED, 0);
                 lcd.clear(); // LIMPA O DISPLAY
                 lcd.print("Senha Incorreta!!!");
                 delay(2000); // DELAY DE 5 SEGUNDOS
@@ -102,6 +135,11 @@ void loop() {
               }
               break;
       case 'B':
+            digitalWrite(BUZ, HIGH); // ativa o SOM
+            analogWrite (LEDBLUE, 255);
+            delay(500);            // espera por um segundo
+            digitalWrite(BUZ, LOW); // Desativa o SOM
+            analogWrite (LEDBLUE, 0);
     
                 if(i <= 0 ){
                 i = 0;
@@ -131,6 +169,11 @@ void loop() {
                 }
               break;
       case 'C':
+            digitalWrite(BUZ, HIGH); // ativa o SOM
+            analogWrite (LEDBLUE, 255);
+            delay(500);            // espera por um segundo
+            digitalWrite(BUZ, LOW); // Desativa o SOM
+            analogWrite (LEDBLUE, 0);
                //limpa a variável que guarda a senha que está sendo digitada
               digitado = "";
               i = 0;
@@ -144,6 +187,11 @@ void loop() {
               //configura a mensagem para digitar a senha
               break;
       case 'D':
+            digitalWrite(BUZ, HIGH); // ativa o SOM
+            analogWrite (LEDBLUE, 255);
+            delay(500);            // espera por um segundo
+            digitalWrite(BUZ, LOW); // Desativa o SOM
+            analogWrite (LEDBLUE, 0);
               digitado = "";
               i = 0;
               lcd.clear(); // LIMPA O DISPLAY
@@ -173,7 +221,12 @@ void loop() {
             digitado+=teclado;
             Serial.println("Digitado: "+digitado+" I: "+i);
             //imrpime na tela o símbolo pressionado
-          
+            
+            digitalWrite(BUZ, HIGH); // ativa o SOM
+            analogWrite (LEDRED, 255);
+            delay(100);            // espera por um segundo
+            digitalWrite(BUZ, LOW); // Desativa o SOM
+            analogWrite (LEDRED, 0);
             
             lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
             lcd.print(digitado);
@@ -185,18 +238,6 @@ void loop() {
   
   }
   }
-    
-  //lcd.print(teclado);
 
-    
-    /*  lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
-      lcd.print("Aguardando.)");
-      delay(1000); // DELAY DE 2 SEGUNDOS
-      lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
-      lcd.print("Aguardando..)");
-      delay(1000); // DELAY DE 2 SEGUNDOS
-      lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
-      lcd.print("Aguardando...)");
-      delay(1000); // DELAY DE 2 SEGUNDOS*/
     
 }
